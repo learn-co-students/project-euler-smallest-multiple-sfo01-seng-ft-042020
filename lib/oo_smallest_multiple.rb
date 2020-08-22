247eeb6e7345c3b57c4a5a5e4a3a1b68ef710171
Don Mallory <dmm333@gmail.com>
2020-08-22 15:13:51 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,39 @@
-# Implement your object-oriented solution here!
+def smallest_multiple(input)
+    needed_factors = {}
+    for i in 2..input
+        needed = find_factors(i)
+        needed.uniq.each do |f|
+            count = needed.count(f)
+            if !needed_factors[f] || needed_factors[f] < count
+                needed_factors[f] = count
+            end
+        end
+    end
+    product = 1
+    needed_factors.each_pair do |k, v|
+        product = product * k ** v
+    end
+    product
+end
+
+def find_factors(num)
+    drain = num
+    factors = []
+    loop do
+        factor = find_factor(drain)
+        if factor
+            drain = drain / factor
+            factors << factor
+        end
+        break if drain == 1
+    end
+    factors
+end
+
+def find_factor(num)
+    for i in 2..num
+        if num % i == 0
+            return i
+        end
+    end
+end
